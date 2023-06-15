2d0a361a221d789a3862ed224dad0bd5fa6dc2eb
redznn <103375250+redznn@users.noreply.github.com>
2023-06-14 23:50:16 -0300

    vendorsetup.sh: Add

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+echo "Cloning Max Shot Device Tree and stuff"
+git clone https://github.com/GuilianoS/kernel_asus_A001D.git -b lineage-18.1 kernel/asus/A001D
+git clone https://github.com/GuilianoS/vendor_asus_A001D.git -b lineage-18.1 vendor/asus/A001D
