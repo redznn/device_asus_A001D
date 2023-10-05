87a49831037f5fddc91abb13d1a899116884f5b8
redznn <h908659@gmail.com>
2023-10-04 21:23:37 -0300

    A001D: Build android.hardware.radio.config@1.2.vendor CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.config@1.2.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)
    Signed-off-by: redznn <h908659@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libcnefeatureconfig \
