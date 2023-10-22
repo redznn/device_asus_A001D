072d8af9cdfcebab8de8ddd1e3b479310843da4c
redznn <h908659@gmail.com>
2023-10-22 12:39:03 -0300

    A001D: Drop lineage livedisplay and lineage power
    Signed-off-by: redznn <h908659@gmail.com>

--- device.mk
+++ device.mk
@@ -177,10 +177,6 @@ PRODUCT_PACKAGES += \
 		libstdc++.vendor \
 		Snap \
 
-# Lineage livedisplay
-PRODUCT_PACKAGES += \
-		vendor.lineage.livedisplay@2.0-service-sdm
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sysconfig/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
