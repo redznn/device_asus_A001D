b879bcf87e186cf6c43e235f404825bdbd8493ad
Bruno Martins <bgcngm@gmail.com>
2023-10-22 12:19:20 -0300

    A001D: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Signed-off-by: redznn <h908659@gmail.com>

--- device.mk
+++ device.mk
@@ -87,7 +87,13 @@ PRODUCT_COPY_FILES += \
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
-    
+
+
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService 
