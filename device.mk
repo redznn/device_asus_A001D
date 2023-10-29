af043c68b0b649dd7908077ab0c35a25e1479c13
hraj9258 <himanshurajkush@gmail.com>
2023-10-29 14:15:12 -0300

    A001D: Build android.hardware.drm@1.1.so * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.2-service.widevine": library "android.hardware.drm@1.1.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -220,10 +220,11 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.0-service \
-		android.hardware.drm@1.0-impl \
-		android.hardware.drm@1.1-service.clearkey \
-		android.hardware.drm@1.1-service.widevine
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
 
 # FM
 PRODUCT_PACKAGES += \
