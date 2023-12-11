da26af8966e50b5c22a9038f0d0feb0349a013d9
criperm19 <96837536+criperm19@users.noreply.github.com>
2023-12-11 15:46:46 -0300

    Update arrow_A001D.mk

--- arrow_A001D.mk
+++ arrow_A001D.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
-PRODUCT_NAME := arrow_A001D
+PRODUCT_NAME := banana_A001D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_A001D
 PRODUCT_MANUFACTURER := asus
